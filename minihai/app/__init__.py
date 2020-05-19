56f2fdf7a8eb249a9dcf37391833090bc2ea6ae0
Aarni Koskela <akx@iki.fi>
2020-05-20 00:59:53 +0300

    Publish data dir

--- minihai/app/__init__.py
+++ minihai/app/__init__.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
 from .routers import misc, commits, executions
+from minihai import conf
 
 app = FastAPI()
 app.include_router(misc.router)
 app.include_router(commits.router)
 app.include_router(executions.router)
+app.mount("/data", StaticFiles(directory=conf.settings.data_path), name="data")
